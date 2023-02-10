2f679b15566f15e0ebd452c6e3d567951ca1d2a1
Ramkumar <ramkumar0495@gmail.com>
2023-02-10 10:02:13 +0530

    login functionality

---
+++ src/test/java/org/test/Sample1.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Sample1 {
+	private void test1() {
+		// TODO Auto-generated method stub
+     System.out.println("a");
+	}
+	private void test2() {
+		// TODO Auto-generated method stub
+		System.out.println("b");
+
+	}
+
+}
